938c0a27845a5e95d24a39bcc0aa97df8e2e3b2f
NXWBAH <williams_nathaniel@bah.com>
2022-08-11 07:38:37 -0400

    Navbar and home component impl

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-	const title = 'Welcome to the new Blog!';
-	const likes = 50;
-	// const person = { name: 'Yoshi', age: 30 };
 	const googleURL = 'http://www.google.com';
 
 	return (
 		<div className='App'>
+			<Navbar />
 			<div className='content'>
-				<h1>{title}</h1>
-				<p>Liked {likes} times</p>
-				<p>{10}</p>
-				<p>{'Hello Ninjas'}</p>
-				<p>{[1, 2, 3, 4, 5]}</p>
-				<p>{Math.random() * 10}</p>
-
+				<Home />
 				<a href={googleURL}>Google Site</a>
 			</div>
 		</div>
