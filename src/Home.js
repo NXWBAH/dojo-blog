b672fdfd43f72a417eb6ee109d8ad5477450e9e3
NXWBAH <williams_nathaniel@bah.com>
2022-08-11 08:26:30 -0400

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,29 @@
 const Home = () => {
+	const handleClick = () => {
+		console.log('Hello Ninjas');
+	};
+
+	const handleClickAgain = (name) => {
+		console.log('Hello' + name);
+	};
+
 	return (
 		<div className='home'>
 			<h2>HomePage</h2>
+			<button onClick={handleClick}>Click me!</button>
+
+			<button
+				onClick={() => {
+					handleClickAgain('mario');
+				}}>
+				Click me Again!
+			</button>
 		</div>
 	);
 };
+
+// Note: We don't want to invoke the function handleClick.
+// If we do handleClick(), the function would run at runtime as opposed to running when the button is clicked.
+// the second button is an anonymous button that redirects to another function. This is used when you have variables that you want to pass in
 
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,16 @@ const navbar = () => {
 			<h1>The Dojo Blog</h1>
 			<div className='links'>
 				<a href='/'>Home</a>
-				<a href='/crate'>New Blog</a>
+				<a
+					href='/create'
+					style={{ color: 'white', backgroundColor: '#f1356d', borderRadius: '8px' }}>
+					New Blog
+				</a>
 			</div>
 		</nav>
 	);
 };
+
+// Style {{}} = Outer means Dynamic Value, Inner means JS object
 
 export default navbar;
