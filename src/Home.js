15aabb712693a3863eb39fbc530adc851c09d8d0
NXWBAH <williams_nathaniel@bah.com>
2022-08-11 08:46:21 -0400

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,42 @@
+import { useState } from 'react';
+
 const Home = () => {
+	// let name  = "mario";
+	// When using useState, you need to attach it to an array variable.
+	// Name = the current variable of the state, setName = the function that will be used to set the new Name when the function is called.
+	const [name, setName] = useState('Mario');
+	const [age, setAge] = useState(25);
 	const handleClick = () => {
-		console.log('Hello Ninjas');
+		setName('Luigi');
+		setAge(30);
 	};
 
-	const handleClickAgain = (name) => {
-		console.log('Hello' + name);
-	};
+	// *** Anonymous Function ***
+	// const handleClickAgain = (name) => {
+	// 	console.log('Hello' + name);
+	// };
 
 	return (
 		<div className='home'>
 			<h2>HomePage</h2>
+
+			<p>
+				{name} is {age} years old
+			</p>
 			<button onClick={handleClick}>Click me!</button>
 
-			<button
+			{/* <button
 				onClick={() => {
 					handleClickAgain('mario');
 				}}>
 				Click me Again!
-			</button>
+			</button> */}
 		</div>
 	);
 };
 
 // Note: We don't want to invoke the function handleClick.
 // If we do handleClick(), the function would run at runtime as opposed to running when the button is clicked.
-// the second button is an anonymous button that redirects to another function. This is used when you have variables that you want to pass in
+// anonymous button redirects to another function. This is used when you have variables that you want to pass in
 
 export default Home;
