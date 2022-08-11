3443d420ea034433ce8210313309a7ae663eec97
NXWBAH <williams_nathaniel@bah.com>
2022-08-11 07:53:47 -0400

    Applying generic CSS Styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-	const googleURL = 'http://www.google.com';
-
 	return (
 		<div className='App'>
 			<Navbar />
 			<div className='content'>
 				<Home />
-				<a href={googleURL}>Google Site</a>
 			</div>
 		</div>
 	);
