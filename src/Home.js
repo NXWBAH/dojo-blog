65d3b79eb1cc361a6823293c0f19ebb9b7924acb
NXWBAH <williams_nathaniel@bah.com>
2022-08-12 06:50:23 -0400

    Lesson 13 - functions as Props

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,25 @@ const Home = () => {
 		{ title: 'Web Dev Top Tips!', body: 'lorem ipsum...', author: 'Mario', id: 3 },
 	]);
 
+	// This function passes the entire blogs array into the new Blogs, but looks for the blog associated with the id passed into it.
+	// That blog is then removed from the array --  and the new array, without that blog, is set as the updated blogs to be displayed.
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlogs(newBlogs);
+	};
+
 	//  Props are used here for two reasons
 	// 1. It allows for the BlogList component to be more re-useable
 	// 2. It allows for us to still use the data above in the home component
-	// Props are used to pass data from a parent component to a child component.
-	// Create a tag on the Component, and then access it wihtin the component's file.
-	// Props here, are both 'blogs' and 'title'
+	// 		Props are used to pass data from a parent component to a child component.
+	// 		Create a tag on the Component, and then access it wihtin the component's file.
+	// 		Props here, are both 'blogs' and 'title'
+	// .Filter allows you to cycle through the array and filter what you want to be returned.
+	// blogs.filter((blog) => blog.author === 'mario') allows you only return Blogs associated with mario
 	return (
 		<div className='home'>
-			<BlogList blogs={blogs} title='All Blogs!' />
+			<BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+			<BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs!" />
 		</div>
 	);
 };
