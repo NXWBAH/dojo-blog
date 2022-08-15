a2ad303a9541043420a045a4a66e9bbd934ddadd
NXWBAH <williams_nathaniel@bah.com>
2022-08-15 08:55:53 -0400

    Implementing diff buttons for the ispending

--- src/Create.js
+++ src/Create.js
@@ -4,14 +4,33 @@ import { useState } from 'react';
 // - Create the const for each piece of data that is within the form.
 // - the onChange function will be used to set the input to the current state of the variable.
 // - Down at the bottom, the const is output as a preview as the user inputs what they want.
+// - handleSubmit will be used to handle the form when the button is clicked
+// - preventDefault ensures that the data that is input via the user is persisted on the page after clicking the button.
 const Create = () => {
 	const [title, setTitle] = useState('');
 	const [body, setBody] = useState('');
 	const [author, setAuthor] = useState('Mario');
+	const [isPending, setIsPending] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const blog = { title, body, author };
+
+		setIsPending(true);
+
+		fetch('http://localhost:8000/blogs', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(blog),
+		}).then(() => {
+			console.log('New Blog Added');
+			setIsPending(false);
+		});
+	};
 	return (
 		<div className='create'>
 			<h2>Add a New Blog</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>Blog Title:</label>
 				<input type='text' required value={title} onChange={(e) => setTitle(e.target.value)} />
 
@@ -24,7 +43,10 @@ const Create = () => {
 					<option value='Yoshi'>Yoshi</option>
 				</select>
 
-				<button>Add Blog</button>
+				{/* If isPending is not true, the button will remain clickable. If it's true, the buttone will be disabled. */}
+				{/* THIS IS KEY TO POSSIBLY SOMETHING IN THE ADMIN CONSOLE FOR JOB PROJECT */}
+				{!isPending && <button>Add Blog</button>}
+				{isPending && <button disabled>Adding Blog...</button>}
 
 				<p>{title}</p>
 				<p>{body}</p>
